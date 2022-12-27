ae0fb5333225aa3b22a428a682f68681c398c2c3
Cristina Jura <cristina.jura16@gmail.com>
2022-12-27 17:18:15 +0200

    locations file added

--- src/components/searchLocation.js
+++ src/components/searchLocation.js
@@ -2,57 +2,7 @@ import React from "react";
 import "../App.css";
 import Select from "react-select";
 import { FaSearch } from "react-icons/fa";
-
-const options = [
-  {
-    value: "45.472264 22.811300",
-    label: "Clopotiva Take-off, HD",
-  },
-  {
-    value: "45.495828 22.806300",
-    label: "Clopotiva school-slope, HD",
-  },
-  {
-    value: "45.380549 22.850112",
-    label: "Retezat Peak, HD",
-  },
-  {
-    value: "45.554008 22.973854",
-    label: "Ciopeia - hill, HD",
-  },
-  {
-    value: "45.626761 23.036450",
-    label: "Gantaga - hill, HD",
-  },
-  {
-    value: "45.734482 23.037333",
-    label: "Calan - hill, HD",
-  },
-  {
-    value: "45.859838 23.042844",
-    label: "Magura Uroiului, HD",
-  },
-  {
-    value: "45.388145 23.491146",
-    label: "Parangul Mic, HD",
-  },
-  {
-    value: "45.226978 23.684955",
-    label: "Ranca Take-off, GJ",
-  },
-  {
-    value: "45.174227 23.077664",
-    label: "Plesa Peak, GJ",
-  },
-  {
-    value: "45.244790 23.016945",
-    label: "Arcanu Peak, GJ",
-  },
-  {
-    value: "45.988007 23.593869",
-    label: "Rapa Rosie, AB",
-  },
-];
+import { options } from "../locations";
 
 const SearchLocation = ({ onSearchChange }) => {
   const [searchLocation, setSearchLocation] = React.useState(null);
@@ -60,17 +10,6 @@ const SearchLocation = ({ onSearchChange }) => {
   const handleChange = (selectSpot) => {
     setSearchLocation(selectSpot);
     onSearchChange(selectSpot);
-  };
-
-  const loadOptions = (options) => {
-    return {
-      options: options.map((loc) => {
-        return {
-          value: `${loc.latitude} ${loc.longitude}`,
-          label: `${loc.name}`,
-        };
-      }),
-    };
   };
 
   return (
@@ -84,7 +23,6 @@ const SearchLocation = ({ onSearchChange }) => {
         </div>
       }
       options={options}
-      loadOptions={loadOptions}
       onChange={handleChange}
       value={"Location search..."}
     />
---
+++ src/locations.js
@@ -0,0 +1,50 @@
+export const options = [
+  {
+    value: "45.472264 22.811300",
+    label: "Clopotiva Take-off, HD",
+  },
+  {
+    value: "45.495828 22.806300",
+    label: "Clopotiva school-slope, HD",
+  },
+  {
+    value: "45.380549 22.850112",
+    label: "Retezat Peak, HD",
+  },
+  {
+    value: "45.554008 22.973854",
+    label: "Ciopeia - hill, HD",
+  },
+  {
+    value: "45.626761 23.036450",
+    label: "Gantaga - hill, HD",
+  },
+  {
+    value: "45.734482 23.037333",
+    label: "Calan - hill, HD",
+  },
+  {
+    value: "45.859838 23.042844",
+    label: "Magura Uroiului, HD",
+  },
+  {
+    value: "45.388145 23.491146",
+    label: "Parangul Mic, HD",
+  },
+  {
+    value: "45.226978 23.684955",
+    label: "Ranca Take-off, GJ",
+  },
+  {
+    value: "45.174227 23.077664",
+    label: "Plesa Peak, GJ",
+  },
+  {
+    value: "45.244790 23.016945",
+    label: "Arcanu Peak, GJ",
+  },
+  {
+    value: "45.988007 23.593869",
+    label: "Rapa Rosie, AB",
+  },
+];
