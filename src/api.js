fdd7ecf085245dc9124e663e67e83ba7f7654e51
Cristina Jura <cristina.jura16@gmail.com>
2023-02-15 10:18:19 +0200

    reverse geolocation url changed

--- src/api.js
+++ src/api.js
@@ -10,4 +10,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
 export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-export const WEATHER_GEO_API_URL = "http://api.openweathermap.org/geo/1.0";
+export const WEATHER_GEO_API_URL = "https://api.openweathermap.org/geo/1.0";
